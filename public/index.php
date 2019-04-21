b86d777a961fadd6a3505f2abcb478dc00064c49
kamg300 <kamg1134@gmail.com>
2019-04-21 19:54:05 +0100

    Fixed formatting in index.php

--- public/index.php
+++ public/index.php
@@ -4,54 +4,51 @@
 </head>
 
 <body>
-<b>RelationalDatabases</b>
+<b>RelationalDatabases</b><br>
 Relational Databases Part 2 Coursework
-
+<p>
 <b>Requirements</b>
-
+<br>
 MVC Architecture (Non-Framework)          (10%)
-
+<br>
 DAO Design Pattern                        (10%)
-
+<br>
 Singleton Design Pattern                  (5%)
-
+<br>
 Data Validation and Encryption            (5%)
-
+<br>
 Fullfillment of the following Criteria:   (20%)
-
-
-- Registration of a new Customer
-- Purchasing of Films (Relavent data to be inserted into the database)
-- Customers can Edit their personal and account details
-- Customers can view their past purchases
-
+<br><br>
+- Registration of a new Customer<br>
+- Purchasing of Films (Relavent data to be inserted into the database)<br>
+- Customers can Edit their personal and account details<br>
+- Customers can view their past purchases<br>
+</p>
+<p>
 <b>Breaking down of the Requirements</b>
-
-MVC Architecture:
-- Model - View - Controller
-- Seperation as per models from part 1 of this assignment
-
-
-DAO Design Pattern:
-- Data Access Object
-- https://www.journaldev.com/16813/dao-design-pattern
-
-
-Singleton Design Pattern:
-- Only one instance of an object will be created at once
-- https://www.tutorialspoint.com/design_pattern/singleton_pattern.htm
-
-
-Data Validation and Encryption:
-- Password Hashing(?) (password_hash())
-
-
-Criteria:
-- Ability to log-in and register
-- Ability to view all available films
-- Ability to order films (Singular and Plural)
-- Ability to edit account details (Change password, Address, Card Info)
-- Ability to view previous purchases
+<br>
+MVC Architecture:<br>
+- Model - View - Controller<br>
+- Seperation as per models from part 1 of this assignment<br>
+<br><br>
+DAO Design Pattern:<br>
+- Data Access Object<br>
+- https://www.journaldev.com/16813/dao-design-pattern<br>
+<br>
+Singleton Design Pattern:<br>
+- Only one instance of an object will be created at once<br>
+- https://www.tutorialspoint.com/design_pattern/singleton_pattern.htm<br>
+<br><br>
+Data Validation and Encryption:<br>
+- Password Hashing(?) (password_hash())<br>
+<br><br>
+Criteria:<br>
+- Ability to log-in and register<br>
+- Ability to view all available films<br>
+- Ability to order films (Singular and Plural)<br>
+- Ability to edit account details (Change password, Address, Card Info)<br>
+- Ability to view previous purchases<br>
+</p>
 </body>
 
 </html>
