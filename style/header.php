58ae12e143c25292c56e412e1320224c99bc8fc7
kamg300 <kamg1134@gmail.com>
2019-04-24 00:20:14 +0100

    Made basket button only appear when items are in the basket

--- style/header.php
+++ style/header.php
@@ -1,6 +1,7 @@
+<!--  -->
 <div id="header" class="header">
     <div class="login-status">
-        <?php
+        /** @noinspection UnknownInspectionInspection */<?php
         include '../controllers/SessionController.php';
         setupSession();
         if (isset($_SESSION['email'], $_SESSION['logged_in']) && $_SESSION['logged_in']) {
@@ -15,7 +16,9 @@
         <?php
         echo '<form action="index.php"><input type="submit" value="Homepage"></form>';
         if (isset($_SESSION['email'], $_SESSION['logged_in']) && $_SESSION['logged_in']) {
-            echo '<form action="basket.php"><input type="submit" value="View Basket"></form>';
+            if (isset($_SESSION['basket']) && !empty($_SESSION['basket'])) {
+                echo '<form action="basket.php"><input type="submit" value="View Basket"></form>';
+            }
             echo '<form action="../controllers/LogoutController.php"><input type="submit" value="Logout"></form>';
         } else {
             echo '<form action="login.php"><input type="submit" value="Login"></form>';
