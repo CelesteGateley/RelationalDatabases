e5318d67f6364ebd8dfb4858d95ed78c1b575844
kamg300 <kamg1134@gmail.com>
2019-04-28 14:13:41 +0100

    Moved the film table from index.php to its own file

---
+++ controllers/FilmTable.php
@@ -0,0 +1,37 @@
+<div class="film-table">
+    <table class="product-table">
+        <tr>
+            <th>Film Name</th>
+            <th>Description</th>
+            <th>Rating</th>
+            <?php if (isset($_SESSION['logged_in']) && $_SESSION['logged_in']) { echo '<th>Stock</th>'; echo '<th>Add to Basket</th>'; } ?>
+        </tr>
+        <?php
+            verifySession();
+            foreach ($_SESSION['films']->getAllFilms() as $film) {
+                $stock = $film->getStock();
+                if (isset($_SESSION['basket'][$film->getId()])) {
+                    $stock -= $_SESSION['basket'][$film->getId()];
+                }
+                if ($stock <= 0) { echo '<tr style="color: red;">';}
+                else { echo '<tr>'; }
+                echo '<td>' . $film->getName() . '</td>';
+                echo '<td>' . $film->getDescription() . '</td>';
+                echo '<td>' . $_SESSION['films']->getRatingKey()[$film->getRatingId()] . '</td>';
+                if (isset($_SESSION['logged_in']) && $_SESSION['logged_in']) {
+                    echo '<td>' . $stock . '</td>';
+                    if ($stock <= 0) { echo '<td>N/A</td>'; }
+                    else {
+                        echo '<td><form method="post" action="../controllers/BasketController.php">
+                               <input type="hidden" name="id" value="' . $film->getId() . '">
+                               <input type="hidden" name="method" value="add">
+                               <input type="submit" value="Add">
+                               </form>
+                          </td>';
+                    }
+                }
+                echo '</tr>';
+            }
+        ?>
+    </table>
+</div>
